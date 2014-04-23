0be82694985850721c7730395bfb8f50db95eb22
matthova <matthova87@gmail.com>
2014-04-23 09:07:12 -0500

    just because

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def session_status(current_user)
     if current_user
       render :partial => "shared/session_status", locals: { current_user: current_user }
+    else
+      render :partial => "shared/login_prompt"
     end
   end
   
